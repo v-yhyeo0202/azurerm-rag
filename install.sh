ac65c50ab82d522f4b4dc922e28a158af00d5835
v-yhyeo0202 <v-yyeo@microsoft.com>
2026-09-13 13:35:14 +0800

    Add installation script, `terraform-provider-azurerm` submodule, and builder Python `requirements.txt`

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git submodule add https://github.com/hashicorp/terraform-provider-azurerm
+
+# cd chunk-line-extractor
+# go mod init chunk-line-extractor
+# go mod tidy
+# go mod vendor
+# go build
+
+# cd ../builder
+# python -m venv venv-azurerm-rag-builder
+# source venv-azurerm-rag-builder/bin/activate
+# pip install -r requirements.txt
